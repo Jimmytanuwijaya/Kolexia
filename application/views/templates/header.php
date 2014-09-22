5f98b846331e4e11ed8965a922faa09c8787ebc0
Septian Rahman <tianboyand97@gmail.com>
2014-09-22 11:59:24 +0700

    Pindah folder images to assets

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,13 +1,13 @@
 <html>
 <head>
 	<title>Kolexia | <?php echo $title ?></title>
-	<link rel="stylesheet" type="text/css" href="home.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	
 </head>
 <body>
 <div id="pu">
 	<div class= "nav-pu">
-		<img src="images/logo2.png" width="200px" height="150px" class="logo">
+		<img src="assets/images/logo2.png" width="200px" height="150px" class="logo">
 		<h5 class="logo-desc">The #1 Social MarketPlace for Indonesian Handmade in Indonesia</h5>
 	</div>
 </div>
